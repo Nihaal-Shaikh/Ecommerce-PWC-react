aff53f2cf936faa91b752a1dc8eeca46111e78b4
Shaikh Nihaal <nihaal@sapnagroup.com>
2023-08-27 20:04:45 +0530

    Product collection component done

---
+++ src/components/home/Collection.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+function Collection() {
+    return (
+        <>
+            <Container>
+                <div className='section-title text-center mb-55'>
+                    <h2>PRODUCT COLLECTION</h2>
+                    <p>Some of our Exclusive Collections.</p>
+                </div>
+                <Row>
+                    <Col className='p-0' xl={3} lg={3} md={3} sm={6} xs={6}>
+                        <Card className='image-box card w-100'>
+                            <img className='center w-75' src="https://rukminim2.flixcart.com/image/832/832/xif0q/t-shirt/r/v/k/m-ausk0165-ausk-original-imaghu9fbhcgf2za.jpeg?q=70" alt="" />
+                            <Card.Body>
+                                <p className='product-name-on-card'>Men Striped Round Neck Cotton Blend Blue, Black T-Shirt</p>
+                                <p className='product-price-on-card'>Price: $150</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className='p-0' xl={3} lg={3} md={3} sm={6} xs={6}>
+                        <Card className='image-box card w-100'>
+                            <img className='center w-75' src="https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/z/i/6/m-a2atw139-tommy-hilfiger-original-imagk4enwa9vxkfj.jpeg?q=70" alt="" />
+                            <Card.Body>
+                                <p className='product-name-on-card'>Men Regular Fit Striped Button Down Collar Casual Shirt</p>
+                                <p className='product-price-on-card'>Price: $150</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className='p-0' xl={3} lg={3} md={3} sm={6} xs={6}>
+                        <Card className='image-box card w-100'>
+                            <img className='center w-75' src="https://rukminim2.flixcart.com/image/832/832/xif0q/t-shirt/4/c/l/-original-imagsbygt7cnkghy.jpeg?q=70" alt="" />
+                            <Card.Body>
+                                <p className='product-name-on-card'>Men Printed Round Neck Pure Cotton Blue T-Shirt</p>
+                                <p className='product-price-on-card'>Price: $150</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className='p-0' xl={3} lg={3} md={3} sm={6} xs={6}>
+                        <Card className='image-box card w-100'>
+                            <img className='center w-75' src="https://rukminim2.flixcart.com/image/832/832/xif0q/t-shirt/z/g/e/-original-imagpqjxyad3gjby.jpeg?q=70" alt="" />
+                            <Card.Body>
+                                <p className='product-name-on-card'>Men Printed V Neck Polyester Green T-Shirt</p>
+                                <p className='product-price-on-card'>Price: $150</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className='p-0' xl={3} lg={3} md={3} sm={6} xs={6}>
+                        <Card className='image-box card w-100'>
+                            <img className='center w-75' src="https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/m/s/o/m-usshto2367-u-s-polo-assn-original-imagsn4z32mdn8ru.jpeg?q=70" alt="" />
+                            <Card.Body>
+                                <p className='product-name-on-card'>Men Tailored Fit Printed Cut Away Collar Casual Shirt</p>
+                                <p className='product-price-on-card'>Price: $150</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className='p-0' xl={3} lg={3} md={3} sm={6} xs={6}>
+                        <Card className='image-box card w-100'>
+                            <img className='center w-75' src="https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/4/0/7/-original-imags44yx8ez8m2r.jpeg?q=70" alt="" />
+                            <Card.Body>
+                                <p className='product-name-on-card'>Men Slim Fit Striped Formal Shirt</p>
+                                <p className='product-price-on-card'>Price: $150</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className='p-0' xl={3} lg={3} md={3} sm={6} xs={6}>
+                        <Card className='image-box card w-100'>
+                            <img className='center w-75' src="https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/b/w/o/s-c301-white-dennis-lingo-original-imafw2ggrk5epcfu-bb.jpeg?q=70" alt="" />
+                            <Card.Body>
+                                <p className='product-name-on-card'>Men Slim Fit Solid Spread Collar Casual Shirt</p>
+                                <p className='product-price-on-card'>Price: $150</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className='p-0' xl={3} lg={3} md={3} sm={6} xs={6}>
+                        <Card className='image-box card w-100'>
+                            <img className='center w-75' src="https://rukminim2.flixcart.com/image/832/832/xif0q/shirt/n/f/q/-original-imagscygsaehn6uk.jpeg?q=70" alt="" />
+                            <Card.Body>
+                                <p className='product-name-on-card'>Men Slim Fit Printed Spread Collar Casual Shirt</p>
+                                <p className='product-price-on-card'>Price: $150</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
+
+export default Collection
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import FeaturedProducts from '../components/home/FeaturedProducts'
 import Categories from '../components/home/Categories'
+import Collection from '../components/home/Collection'
 
 function Homepage() {
   return (
     <>
     <FeaturedProducts />
+    <Collection />
     <Categories />
     </>
   )
