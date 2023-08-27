bdd2a000688bb2271d858edd3d21349094f9f5c6
Shaikh Nihaal <nihaal@sapnagroup.com>
2023-08-27 19:16:31 +0530

    Categories component done

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import FeaturedProducts from '../components/home/FeaturedProducts'
+import Categories from '../components/home/Categories'
 
 function Homepage() {
   return (
     <>
     <FeaturedProducts />
+    <Categories />
     </>
   )
 }
