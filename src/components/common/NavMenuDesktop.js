0d40ddaea4df7f2d6b5424d0072273f76e2847f5
Shaikh Nihaal <nihaal@sapnagroup.com>
2023-08-30 17:18:57 +0530

    Started with Nav Menu for Desktop

--- src/components/common/NavMenuDesktop.js
+++ src/components/common/NavMenuDesktop.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Navbar, Container, Row, Col } from "react-bootstrap";
+import Logo from "../../assets/images/easyshop.png"
+import { Link } from "react-router-dom"
 
 function NavMenuDesktop() {
-  return (
-    <div>NavMenuDesktop</div>
-  )
+    return (
+        <>
+            <div className='TopSectionDown'>
+                <Navbar fixed='top' className='navbar' bg="light">
+                    <Container fluid={true}>
+                        <Row>
+                            <Col lg={4} md={4} sm={12} xs={12}>
+                                <Link to="/"><img className='nav-logo' src={Logo} alt="" /></Link>
+                            </Col>
+                            <Col lg={4} md={4} sm={12} xs={12}>
+                            </Col>
+                            <Col lg={4} md={4} sm={12} xs={12}>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Navbar>
+            </div>
+        </>
+    )
 }
 
 export default NavMenuDesktop
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,15 +4,17 @@ import Categories from '../components/home/Categories'
 import Collection from '../components/home/Collection'
 import NewArrivals from '../components/home/NewArrivals'
 import HomeTop from '../components/home/HomeTop'
+import NavMenuDesktop from '../components/common/NavMenuDesktop'
 
 function Homepage() {
     return (
         <>
+            <NavMenuDesktop />
             <HomeTop />
             <FeaturedProducts />
             <NewArrivals />
-            <Collection />
             <Categories />
+            <Collection />
         </>
     )
 }
