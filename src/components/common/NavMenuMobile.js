9f58545cfbcb28a0b624577f3a5dfd40d6cf3d37
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-01 21:03:37 +0530

    NavMenuMobile.js formatting done

--- src/components/common/NavMenuMobile.js
+++ src/components/common/NavMenuMobile.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function NavMenuMobile() {
-  return (
-    <div>NavMenuMobile</div>
-  )
+    return (
+        <div>NavMenuMobile</div>
+    )
 }
 
 export default NavMenuMobile
