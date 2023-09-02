018493a1488933e5dd2775c38febe59b6a9ac664
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-02 12:11:11 +0530

    Working on mobile view menu

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,11 +5,12 @@ import Collection from '../components/home/Collection'
 import NewArrivals from '../components/home/NewArrivals'
 import HomeTop from '../components/home/HomeTop'
 import NavMenuDesktop from '../components/common/NavMenuDesktop'
+import NavMenuMobile from '../components/common/NavMenuMobile'
 
 function Homepage() {
     return (
         <>
-            <NavMenuDesktop />
+            <NavMenuMobile />
             <HomeTop />
             <FeaturedProducts />
             <NewArrivals />
