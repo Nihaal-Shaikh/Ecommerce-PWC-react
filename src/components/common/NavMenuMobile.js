c1b9757b52f8c30d8e5018113db50f14304fdaaf
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-02 12:44:28 +0530

    Working on mobile view menu

--- src/components/common/NavMenuMobile.js
+++ src/components/common/NavMenuMobile.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Container, Row, Col, Button } from "react-bootstrap";
 import Logo from "../../assets/images/easyshop.png"
 import { Link } from "react-router-dom"
 
 function NavMenuMobile() {
+
+    const [sideNavState, setSideNavState] = useState('sideNavClose');
+    const [contentOverState, setContentOverState] = useState('ContentOverlayClose');
+  
+    const sideNavOpenClose = () => {
+      if (sideNavState === 'sideNavOpen') {
+        setSideNavState('sideNavClose');
+        setContentOverState('ContentOverlayClose');
+      } else {
+        setSideNavState('sideNavOpen');
+        setContentOverState('ContentOverlayOpen');
+      }
+    };
+
     return (
         <>
         <div className='TopSectionDown'>
                     <Container fluid={'true'} className='fixed-top shadow-sm p-2 mb-0 bg-white'>
                         <Row>
                             <Col lg={4} md={4} sm={12} xs={12}>
-                                <Button className='btn'><i className='fa fa-bars'></i></Button>
+                                <Button className='btn' onClick={sideNavOpenClose}><i className='fa fa-bars'></i></Button>
                                 <Link to="/">{' '}<img className='nav-logo' src={Logo} alt="" />{' '}</Link>
                                 <Button className='cart-btn'><i className='fa fa-shopping-cart'></i>3 items</Button>
                             </Col>
                         </Row>
                     </Container>
-                    <div className='sideNavOpen'>
+                    <div className={sideNavState}>
                         <hr className='w-80' />
                         <div className='list-group'>
                             <a href="" className='list-group-item nav-font nav-itemmenu list-group-item-action'><i className='fa mr-2 fa-home'>Home</i></a>
                         </div>
                     </div>
-                    <div className='ContentOverlayOpen'>
+                    <div onClick={sideNavOpenClose} className={contentOverState}>
 
                     </div>
             </div>
