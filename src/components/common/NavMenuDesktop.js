7ad220a7f57f29d0a9d8bcce58b6db9f46a2ad99
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-03 21:56:46 +0530

    Nav icon updated

--- src/components/common/NavMenuDesktop.js
+++ src/components/common/NavMenuDesktop.js
@@ -3,6 +3,7 @@ import { Navbar, Container, Row, Col, Button } from "react-bootstrap";
 import Logo from "../../assets/images/easyshop.png";
 import { Link } from "react-router-dom";
 import MegaMenuAll from '../home/MegaMenuAll';
+import Bars from "../../assets/images/bars.png"
 
 function NavMenuDesktop() {
 
@@ -26,7 +27,7 @@ function NavMenuDesktop() {
                     <Container fluid={'true'} className='fixed-top shadow-sm p-2 mb-0 bg-white'>
                         <Row>
                             <Col lg={4} md={4} sm={12} xs={12}>
-                                <Button className='btn' onClick={sideNavOpenClose}><i className='fa fa-bars'></i></Button>
+                                <img src={Bars} onClick={sideNavOpenClose} className='bar-img' alt="" />
                                 <Link to="/">{' '}<img className='nav-logo' src={Logo} alt="" />{' '}</Link>
                             </Col>
                             <Col className='p-1 mt-1' lg={4} md={4} sm={12} xs={12}>
