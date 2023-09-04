b5f668c96b56b880ac8d51da9806c63b77cff869
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-04 15:29:46 +0530

    Cart list complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -14,7 +14,7 @@ function Cart() {
                             <Card.Body>
                                 <Row>
                                     <Col md={3} lg={3} sm={6} xs={6}>
-                                        <img className="w-100 h-100" src={Product1} />
+                                        <img className="cart-product-img" src={Product1} />
                                     </Col>
 
                                     <Col md={6} lg={6} sm={6} xs={6}>
@@ -36,7 +36,7 @@ function Cart() {
                             <Card.Body>
                                 <Row>
                                     <Col md={3} lg={3} sm={6} xs={6}>
-                                        <img className="w-100 h-100" src={Product1} />
+                                        <img className="cart-product-img" src={Product1} />
                                     </Col>
 
                                     <Col md={6} lg={6} sm={6} xs={6}>
@@ -58,7 +58,7 @@ function Cart() {
                             <Card.Body>
                                 <Row>
                                     <Col md={3} lg={3} sm={6} xs={6}>
-                                        <img className="w-100 h-100" src={Product1} />
+                                        <img className="cart-product-img" src={Product1} />
                                     </Col>
 
                                     <Col md={6} lg={6} sm={6} xs={6}>
@@ -81,7 +81,7 @@ function Cart() {
                             <Card.Body>
                                 <Row>
                                     <Col md={3} lg={3} sm={6} xs={6}>
-                                        <img className="w-100 h-100" src={Product1} />
+                                        <img className="cart-product-img" src={Product1} />
                                     </Col>
 
                                     <Col md={6} lg={6} sm={6} xs={6}>
@@ -94,6 +94,19 @@ function Cart() {
                                         <input placeholder="2" className="form-control text-center" type="number" />
                                         <Button className="btn btn-block w-100 mt-3  site-btn"><i className="fa fa-trash-alt"></i> Remove </Button>
 
+                                    </Col>
+                                </Row>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className="p-1" lg={12} md={12} sm={12} xs={12} >
+                        <Card >
+                            <Card.Body>
+                                <Row>
+                                    <Col md={4} lg={4} sm={6} xs={6}>
+                                        <h5> Total Items = 05 </h5>
+                                        <h5>Total Price = 5000$</h5>
+                                        <Button className="btn btn-block w-100 mt-3  site-btn"><i className="fa fa-shopping-cart"></i> Checkout </Button>
                                     </Col>
                                 </Row>
                             </Card.Body>
