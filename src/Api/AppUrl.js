e9ed0019f24842858958fb23a6364cc8592220ff
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-12 21:37:45 +0530

    About us text fetched from db

--- src/Api/AppUrl.js
+++ src/Api/AppUrl.js
@@ -1,11 +1,13 @@
 const getBaseURL = 'http://127.0.0.1:8000/api';
 const getVisitorDetailsURL = `${getBaseURL}/getVisitor`;
 const postContact = `${getBaseURL}/postContact`;
+const allSiteInfo = `${getBaseURL}/allSiteInfo`;
 
 const AppUrl = {
     getBaseURL,
     getVisitorDetailsURL,
     postContact,
+    allSiteInfo
 };
 
 export default AppUrl;
