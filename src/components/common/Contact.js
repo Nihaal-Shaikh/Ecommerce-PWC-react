082baa5a18529cb31c7e83a9b09c575b6d4055ee
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-12 13:26:43 +0530

    Working on contact page api

--- src/components/common/Contact.js
+++ src/components/common/Contact.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Container, Row, Col, Button } from "react-bootstrap";
 
 function Contact() {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        alert('Hello');
+    }
+
     return (
         <>
             <Container>
@@ -9,13 +19,13 @@ function Contact() {
                     <Col className='shadow-sm bg-white mt-2' md={12} lg={12} sm={12} xs={12}>
                         <Row className='text-center'>
                             <Col className='d-flex justify-content-center' md={6} lg={6} sm={12} xs={12}>
-                                <Form className='onboardForm'>
+                                <Form onSubmit={onFormSubmit} className='onboardForm'>
                                     <h4 className='section-title-login'>Contact With Us</h4>
                                     <h6 className='section-sub-title'>Please contact with us</h6>
-                                    <input className='form-control m-2' type="text" placeholder='Enter Mobile Number' />
-                                    <input className='form-control m-2' type="email" placeholder='Enter Email Id' />
-                                    <input className='form-control m-2' type="text" placeholder='Enter Your Message' />
-                                    <Button className='btn btn-block m-2 site-btn-login'>Next</Button>
+                                    <input className='form-control m-2' type="text" placeholder='Enter Your Name' onChange={(e) => {setName(e.target.value)}} />
+                                    <input className='form-control m-2' type="email" placeholder='Enter Email Id' onChange={(e) => {setEmail(e.target.value);}} />
+                                    <Form.Control className='form-control m-2' as="textarea" rows={3} placeholder='Message' onChange={(e) => {setMessage(e.target.value);}} />
+                                    <Button type='submit' className='btn btn-block m-2 site-btn-login'>Next</Button>
                                 </Form>
                             </Col>
                             <Col className='p-0 Desktop m-0' md={6} lg={6} sm={6} xs={6}> <br /><br />
