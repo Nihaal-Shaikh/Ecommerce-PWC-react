d0b3801e0f79752f00e842842acb2ad657511ed1
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-12 09:02:18 +0530

    Strict mode commented to avoid re render

--- src/index.js
+++ src/index.js
@@ -9,9 +9,10 @@ import '../src/assets/css/animate.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    // <React.StrictMode>
+    //     <App />
+    // </React.StrictMode>
+    <App />
 );
 
 reportWebVitals();
