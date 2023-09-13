cfc409113230592bac4dced75fe0b56d5aa3facd
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-13 16:41:52 +0530

    About us and privacy text fetched from db

--- src/components/others/About.js
+++ src/components/others/About.js
@@ -2,22 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import AppUrl from '../../Api/AppUrl';
 import axios from 'axios';
+import parse from 'html-react-parser';
 
 function About() {
 
-    const [about, setAbout] = useState('');
+    const [aboutText, setAboutText] = useState('');
 
     useEffect(() => {
         axios.get(AppUrl.allSiteInfo)
           .then(response => {
-            setAbout(response.data[0]['about']);
+            setAboutText(response.data[0]['about']);
           })
           .catch(error => {
             // Handle errors here
           });
-    }, []);      
-
-    console.log(about);
+    }, []);
 
     return (
         <>
@@ -25,7 +24,7 @@ function About() {
                 <Row className='p-2'>
                     <Col className='shadow-sm bg-white mt-2' md={12} lg={12} sm={12} xs={12}>
                         <h4 className='section-title-login'>About Us Page</h4>
-                        <p className='section-title-contact'>{about}</p>
+                        <p className='section-title-contact'>{parse(aboutText)}</p>
                     </Col>
                 </Row>
             </Container>
--- src/components/others/Privacy.js
+++ src/components/others/Privacy.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
+import AppUrl from '../../Api/AppUrl';
+import axios from 'axios';
+import parse from 'html-react-parser';
 
 function Privacy() {
+
+    const [privacyText, setPrivacyText] = useState('');
+
+    useEffect(() => {
+        axios.get(AppUrl.allSiteInfo)
+          .then(response => {
+            setPrivacyText(response.data[0]['privacy']);
+          })
+          .catch(error => {
+            // Handle errors here
+          });
+    }, []);
+
     return (
         <>
             <Container>
                 <Row className='p-2'>
                     <Col className='shadow-sm bg-white mt-2' md={12} lg={12} sm={12} xs={12}>
                         <h4 className='section-title-login'>Privacy Page</h4>
-                        <p className='section-title-contact'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p className='section-title-contact'>{parse(privacyText)}</p>
                     </Col>
                 </Row>
             </Container>
