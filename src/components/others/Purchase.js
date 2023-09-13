ae442023b5111027245cebb7af9490dde2f5f808
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-13 22:17:53 +0530

    Used session storage to store the purchase page data to avoid api call everytime it loads

--- src/components/others/Purchase.js
+++ src/components/others/Purchase.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+import parse from 'html-react-parser';
+import AppUrl from '../../Api/AppUrl';
 
 function Purchase() {
+    
+    const [purchaseText, setPurchaseText] = useState('');
+
+    useEffect(() => {
+
+        const siteInfoPurchase = sessionStorage.getItem('siteInfoPurchase');
+
+        if (siteInfoPurchase == undefined) {
+            axios.get(AppUrl.allSiteInfo)
+            .then(response => {
+              setPurchaseText(response.data[0]['purchase_guide']);
+              sessionStorage.setItem('siteInfoPurchase', response.data[0]['purchase_guide']);
+            })
+            .catch(error => {
+                toast.error('Something went wrong', {
+                    position: 'bottom-center'
+                });
+            });
+        } else {
+            setPurchaseText(siteInfoPurchase);
+        }
+
+    }, []);
+
     return (
         <>
             <Container>
                 <Row className='p-2'>
                     <Col className='shadow-sm bg-white mt-2' md={12} lg={12} sm={12} xs={12}>
                         <h4 className='section-title-login'>Purchase Page</h4>
-                        <p className='section-title-contact'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p className='section-title-contact'>{parse(purchaseText)}</p>
                     </Col>
                 </Row>
+                <ToastContainer />
             </Container>
         </>
     )
