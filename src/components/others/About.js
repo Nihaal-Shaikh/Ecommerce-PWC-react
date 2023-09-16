93297e89c6167006b387f2a89a9449bbe4729cc1
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-16 11:24:38 +0530

    Loading placeholder added in adbout and purchase page

--- src/components/others/About.js
+++ src/components/others/About.js
@@ -7,15 +7,19 @@ import parse from 'html-react-parser';
 function About() {
 
     const [aboutText, setAboutText] = useState('');
+    const [loaderDiv, setLoaderDiv] = useState('');
+    const [mainDiv, setMainDiv] = useState('d-none');
 
     useEffect(() => {
         axios.get(AppUrl.allSiteInfo)
-          .then(response => {
-            setAboutText(response.data[0]['about']);
-          })
-          .catch(error => {
-            // Handle errors here
-          });
+            .then(response => {
+                setAboutText(response.data[0]['about']);
+                setLoaderDiv('d-none');
+                setMainDiv('');
+            })
+            .catch(error => {
+                // Handle errors here
+            });
     }, []);
 
     return (
@@ -23,8 +27,26 @@ function About() {
             <Container>
                 <Row className='p-2'>
                     <Col className='shadow-sm bg-white mt-2' md={12} lg={12} sm={12} xs={12}>
-                        <h4 className='section-title-login'>About Us Page</h4>
-                        <p className='section-title-contact'>{parse(aboutText)}</p>
+                        <div className={loaderDiv}>
+                            <div class="ph-item">
+                                <div class="ph-col-12">
+                                    <div class="ph-row">
+                                        <div class="ph-col-4"></div>
+                                        <div class="ph-col-8 empty"></div>
+                                        <div class="ph-col-6"></div>
+                                        <div class="ph-col-6 empty"></div>
+                                        <div class="ph-col-12"></div>
+                                        <div class="ph-col-12"></div>
+                                        <div class="ph-col-12"></div>
+                                        <div class="ph-col-12"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={mainDiv}>
+                            <h4 className='section-title-login'>About Us Page</h4>
+                            <p className='section-title-contact'>{parse(aboutText)}</p>
+                        </div>
                     </Col>
                 </Row>
             </Container>
--- src/components/others/Purchase.js
+++ src/components/others/Purchase.js
@@ -7,8 +7,11 @@ import parse from 'html-react-parser';
 import AppUrl from '../../Api/AppUrl';
 
 function Purchase() {
-    
+
     const [purchaseText, setPurchaseText] = useState('');
+    const [loaderDiv, setLoaderDiv] = useState('');
+    const [mainDiv, setMainDiv] = useState('d-none');
+
 
     useEffect(() => {
 
@@ -16,17 +19,21 @@ function Purchase() {
 
         if (siteInfoPurchase == undefined) {
             axios.get(AppUrl.allSiteInfo)
-            .then(response => {
-              setPurchaseText(response.data[0]['purchase_guide']);
-              sessionStorage.setItem('siteInfoPurchase', response.data[0]['purchase_guide']);
-            })
-            .catch(error => {
-                toast.error('Something went wrong', {
-                    position: 'bottom-center'
+                .then(response => {
+                    setPurchaseText(response.data[0]['purchase_guide']);
+                    setLoaderDiv('d-none');
+                    setMainDiv('');
+                    sessionStorage.setItem('siteInfoPurchase', response.data[0]['purchase_guide']);
+                })
+                .catch(error => {
+                    toast.error('Something went wrong', {
+                        position: 'bottom-center'
+                    });
                 });
-            });
         } else {
             setPurchaseText(siteInfoPurchase);
+            setLoaderDiv('d-none');
+            setMainDiv('');
         }
 
     }, []);
@@ -36,8 +43,26 @@ function Purchase() {
             <Container>
                 <Row className='p-2'>
                     <Col className='shadow-sm bg-white mt-2' md={12} lg={12} sm={12} xs={12}>
-                        <h4 className='section-title-login'>Purchase Page</h4>
-                        <p className='section-title-contact'>{parse(purchaseText)}</p>
+
+                        <div className={loaderDiv}>
+                            <div class="ph-item">
+                                <div class="ph-col-12">
+                                    <div class="ph-row">
+                                        <div class="ph-col-4"></div>
+                                        <div class="ph-col-8 empty"></div>
+                                        <div class="ph-col-6"></div>
+                                        <div class="ph-col-6 empty"></div>
+                                        <div class="ph-col-12"></div>
+                                        <div class="ph-col-12"></div>
+                                        <div class="ph-col-12"></div>
+                                        <div class="ph-col-12"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={mainDiv}>
+                            <h4 className='section-title-login'>Purchase Page</h4>
+                            <p className='section-title-contact'>{parse(purchaseText)}</p></div>
                     </Col>
                 </Row>
                 <ToastContainer />
