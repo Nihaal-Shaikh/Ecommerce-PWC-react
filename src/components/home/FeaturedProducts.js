faafd3cc6723f2fe339f771af46961e95e526ee5
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-16 21:20:05 +0530

    Featured products in homepage made dynamic

--- src/components/home/FeaturedProducts.js
+++ src/components/home/FeaturedProducts.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import AppUrl from '../../Api/AppUrl';
 
 function FeaturedProducts() {
+
+    const [productData, setProductData] = useState([]);
+
+    useEffect(() => {
+
+        axios.get(AppUrl.ProductListByRemark("FEATURED"))
+            .then(response => {
+                console.log(response);
+                setProductData(response.data);
+            })
+            .catch(error => {
+            });
+    }, []);
+
+    const MyView = productData.map((FeaturedList, i) => {
+        if(!FeaturedList.special_price) {
+            return <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
+            <Link to="/productDetail">
+                <Card className='image-box card'>
+                    <img className='center' src={FeaturedList.image} alt="" />
+                    <Card.Body>
+                        <p className='product-name-on-card'>{FeaturedList.title}</p>
+                        <p className='product-price-on-card'>Price: ${FeaturedList.price}</p>
+                    </Card.Body>
+                </Card>
+            </Link>
+        </Col>
+        } else {
+            return <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
+            <Link to="/productDetail">
+                <Card className='image-box card'>
+                    <img className='center' src={FeaturedList.image} alt="" />
+                    <Card.Body>
+                        <p className='product-name-on-card'>{FeaturedList.title}</p>
+                        <p className='product-price-on-card'>Price: <strike className='text-secondary'>${FeaturedList.special_price}</strike> ${FeaturedList.price}</p>
+                    </Card.Body>
+                </Card>
+            </Link>
+        </Col>
+        }
+    });
+
     return (
         <>
             <Container className='text-center' fluid={true}>
@@ -11,62 +55,7 @@ function FeaturedProducts() {
                     <p>Some of our Exclusive Collections.</p>
                 </div>
                 <Row>
-                    <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
-                        <Link to="/productDetail">
-                        <Card className='image-box card'>
-                            <img className='center' src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/2/n/m/-original-imagna3rjqn8vqxb.jpeg?q=70" alt="" />
-                            <Card.Body>
-                                <p className='product-name-on-card'>Infinix SMART 7 (Emerald Green, 64 GB)</p>
-                                <p className='product-price-on-card'>Price: $150</p>
-                            </Card.Body>
-                        </Card>
-                        </Link>
-                    </Col>
-                    <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
-                        <Card className='image-box card'>
-                            <img className='center' src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/a/6/f/-original-imagnb6nvnqz7hqq.jpeg?q=70" alt="" />
-                            <Card.Body>
-                                <p className='product-name-on-card'>POCO C55 (Forest Green, 64 GB)</p>
-                                <p className='product-price-on-card'>Price: $150</p>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
-                        <Card className='image-box card'>
-                            <img className='center' src="https://rukminim2.flixcart.com/image/416/416/ktketu80/mobile/s/l/c/iphone-13-mlpf3hn-a-apple-original-imag6vzz5qvejz8z.jpeg?q=70" alt="" />
-                            <Card.Body>
-                                <p className='product-name-on-card'>APPLE iPhone 13 (Midnight, 128 GB)</p>
-                                <p className='product-price-on-card'>Price: $150</p>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
-                        <Card className='image-box card'>
-                            <img className='center' src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/z/b/d/-original-imagpgx48f4szdvf.jpeg?q=70" alt="" />
-                            <Card.Body>
-                                <p className='product-name-on-card'>Google Pixel 7a (Sea, 128 GB)</p>
-                                <p className='product-price-on-card'>Price: $150</p>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
-                        <Card className='image-box card'>
-                            <img className='center' src="https://rukminim2.flixcart.com/image/416/416/xif0q/mobile/3/2/r/galaxy-fold5-sm-f946blbhins-samsung-original-imagru5ugghfnwnj.jpeg?q=70" alt="" />
-                            <Card.Body>
-                                <p className='product-name-on-card'>SAMSUNG Galaxy Z Fold5 (Icy Blue, 512 GB)</p>
-                                <p className='product-price-on-card'>Price: $150</p>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col className='p-1' key={1} xl={2} lg={2} sm={4} xs={6}>
-                        <Card className='image-box card'>
-                            <img className='center' src="https://rukminim2.flixcart.com/image/416/416/ksnjp8w0/mobile/f/k/y/galaxy-z-flip3-5g-sm-f711bzkainu-samsung-original-imag662axn2xmght.jpeg?q=70" alt="" />
-                            <Card.Body>
-                                <p className='product-name-on-card'>SAMSUNG Galaxy Z Flip3 5G (Phantom Black, 128 GB)</p>
-                                <p className='product-price-on-card'>Price: $150</p>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                    {MyView}
                 </Row>
             </Container>
         </>
