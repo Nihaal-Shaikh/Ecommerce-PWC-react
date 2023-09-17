9d15aad645411d7be1722788028c1c1a6f3ab379
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-17 18:51:30 +0530

    Home slider made dynamic for mobiles

--- src/components/home/HomeTopMobile.js
+++ src/components/home/HomeTopMobile.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import HomeSlider from './HomeSlider'
+import axios from 'axios';
+import AppUrl from '../../Api/AppUrl';
 
 function HomeTopMobile() {
+
+  const [sliderData, setSliderData] = useState([]);
+
+  useEffect(() => {
+      axios.get(AppUrl.allSlider)
+          .then(response => {
+              setSliderData(response.data);
+          })
+          .catch(error => {
+          });
+  }, []);
   return (
     <>
     <Container className='p-0 m-0 overflow-hidden' fluid={true}>
         <Row className='p-0 m-0 overflow-hidden'>
             <Col lg={12} md={12} sm={12}>
-                {/* <HomeSlider /> */}
+                <HomeSlider data={sliderData} />
             </Col>
         </Row>
     </Container>
