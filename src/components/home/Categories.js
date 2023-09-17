a0c1b30788d24efc3e780cf65703e0c271656515
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-17 22:58:05 +0530

    Loading placeholder added for categories in homepage

--- src/components/home/Categories.js
+++ src/components/home/Categories.js
@@ -3,14 +3,20 @@ import { Container, Row, Col, Card } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import axios from 'axios';
 import AppUrl from '../../Api/AppUrl';
+import CategoryLoading from '../Placeholder/CategoryLoading';
 
 function Categories() {
+
     const [menuData, setMenuData] = useState([]);
+    const [loaderDiv, setLoaderDiv] = useState('');
+    const [mainDiv, setMainDiv] = useState('d-none');
 
     useEffect(() => {
         axios.get(AppUrl.allCategoryDetails)
             .then((response) => {
                 setMenuData(response.data);
+                setLoaderDiv('d-none');
+                setMainDiv('');
             })
             .catch((error) => {
                 // Handle error here
@@ -18,28 +24,31 @@ function Categories() {
     }, []);
 
     return (
-        <div>
-            <Container className="text-center" fluid={true}>
-                <div className="section-title text-center mb-55">
-                    <h2> CATEGORIES</h2>
-                    <p>Some Of Our Exclusive Collection.</p>
-                </div>
-                <Row>
-                    {menuData.map((catList, i) => (
-                        <Col key={i.toString()} className="p-0" xl={2} lg={2} md={2} sm={6} xs={6}>
-                            <Link to={"/productCategory/" + catList.category_name}>
-                            <Card className="h-100 w-100 text-center">
-                                <Card.Body>
-                                    <img className="center" src={catList.category_image} alt={catList.category_name} />
-                                    <h5 className="category-name">{catList.category_name}</h5>
-                                </Card.Body>
-                            </Card>
-                            </Link>
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
-        </div>
+        <>
+            <CategoryLoading loaderDiv={loaderDiv} />
+            <div className={mainDiv}>
+                <Container className="text-center" fluid={true}>
+                    <div className="section-title text-center mb-55">
+                        <h2> CATEGORIES</h2>
+                        <p>Some Of Our Exclusive Collection.</p>
+                    </div>
+                    <Row>
+                        {menuData.map((catList, i) => (
+                            <Col key={i.toString()} className="p-0" xl={2} lg={2} md={2} sm={6} xs={6}>
+                                <Link to={"/productCategory/" + catList.category_name}>
+                                    <Card className="h-100 w-100 text-center">
+                                        <Card.Body>
+                                            <img className="center" src={catList.category_image} alt={catList.category_name} />
+                                            <h5 className="category-name">{catList.category_name}</h5>
+                                        </Card.Body>
+                                    </Card>
+                                </Link>
+                            </Col>
+                        ))}
+                    </Row>
+                </Container>
+            </div>
+        </>
     );
 }
 
