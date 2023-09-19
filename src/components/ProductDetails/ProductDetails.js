fb907006773cfe854c64580e3d84b0f3e9ad6d31
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-19 20:45:45 +0530

    Color, size and quantity dropdowns added in product details page

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,11 +1,46 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
+import Select from 'react-select'
 import { Link } from "react-router-dom";
 
 function ProductDetails(props) {
 
     const productData = props.data;
     const [mainImage, setMainImage] = useState(productData['productDetails'][0]['image_one']);
+    const [selectedColor, setSelectedColor] = useState(null);
+    const [selectedSize, setSelectedSize] = useState(null);
+    const [selectedQuantity, setSelectedQuantity] = useState(null);
+    const quantityArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+    const quantityOptions = quantityArray.map((QuantityList) => ({
+        value: QuantityList,
+        label: QuantityList
+    }));
+
+    let colorOptions = [];
+
+    if(productData['productDetails'][0]['color']) {
+        const colorData = productData['productDetails'][0]['color'];
+        const colorArray = colorData.split(',');
+        colorOptions = colorArray.map((ColorList) => ({
+            value: ColorList,
+            label: ColorList
+        }));
+    }
+
+    let sizeOptions = [];
+    if(productData['productDetails'][0]['size']) {
+        const sizeData = productData['productDetails'][0]['size'];
+        const sizeArray = sizeData.split(',');
+        sizeOptions = sizeArray.map((SizeList) => ({
+            value: SizeList,
+            label: SizeList
+        }));
+    }
+
+    const imgOnClick = (event) => {
+        const imgSrc = event.target.getAttribute('src');
+        setMainImage(imgSrc);
+    };
 
     return (
         <>
@@ -18,16 +53,16 @@ function ProductDetails(props) {
                                 <Container className="my-3">
                                     <Row>
                                         <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                                            <img className="w-100 smallImage product-sm-img" onClick={() => setMainImage(productData['productDetails'][0]['image_one'])} src={productData['productDetails'][0]['image_one']} />
+                                            <img className="w-100 smallImage product-sm-img" onClick={imgOnClick} src={productData['productDetails'][0]['image_one']} />
                                         </Col>
                                         <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                                            <img className="w-100 smallImage product-sm-img" onClick={() => setMainImage(productData['productDetails'][0]['image_two'])} src={productData['productDetails'][0]['image_two']} />
+                                            <img className="w-100 smallImage product-sm-img" onClick={imgOnClick} src={productData['productDetails'][0]['image_two']} />
                                         </Col>
                                         <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                                            <img className="w-100 smallImage product-sm-img" onClick={() => setMainImage(productData['productDetails'][0]['image_three'])} src={productData['productDetails'][0]['image_three']} />
+                                            <img className="w-100 smallImage product-sm-img" onClick={imgOnClick} src={productData['productDetails'][0]['image_three']} />
                                         </Col>
                                         <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                                            <img className="w-100 smallImage product-sm-img" onClick={() => setMainImage(productData['productDetails'][0]['image_four'])} src={productData['productDetails'][0]['image_four']} />
+                                            <img className="w-100 smallImage product-sm-img" onClick={imgOnClick} src={productData['productDetails'][0]['image_four']} />
                                         </Col>
                                     </Row>
                                 </Container>
@@ -44,10 +79,35 @@ function ProductDetails(props) {
                                 <h6 className="mt-2">Sub Category: <b>{productData['productList'][0]['subcategory']}</b></h6>
                                 <h6 className="mt-2">Brand: <b>{productData['productList'][0]['brand']}</b></h6>
                                 <h6 className="mt-2">Product Code: <b>{productData['productList'][0]['product_code']}</b></h6>
-                                <h6 className="mt-2">Quantity</h6>
-                                <input className="form-control text-center w-50" type="number" />
+                                <div className={colorOptions.length > 0 ? '' : 'd-none'}>
+                                    <h6 className="mt-2"> Choose Color </h6>
+                                    <Select
+                                        options={colorOptions}
+                                        value={selectedColor}
+                                        onChange={(value) => setSelectedColor(value)}
+                                        placeholder="Choose Color"
+                                    />
+                                </div>
+                                <div className={sizeOptions.length > 0 ? '' : 'd-none'}>
+                                    <h6 className="mt-2"> Choose Size </h6>
+                                    <Select
+                                        options={sizeOptions}
+                                        value={selectedSize}
+                                        onChange={(value) => setSelectedSize(value)}
+                                        placeholder="Choose Size"
+                                    />
+                                </div>
+                                <div className={quantityOptions.length > 0 ? '' : 'd-none'}>
+                                    <h6 className="mt-2">Choose Quantity</h6>
+                                    <Select
+                                        options={quantityOptions}
+                                        value={selectedQuantity}
+                                        onChange={(value) => setSelectedQuantity(value)}
+                                        placeholder="Choose Quantity"
+                                    />
+                                </div>
 
-                                <div className="input-group mt-3">
+                                <div className="input-group mt-3" style={{ zIndex: 0 }}>
                                     <button className="btn site-btn m-1 "> <i className="fa fa-shopping-cart"></i>  Add To Cart</button>
                                     <button className="btn btn-primary m-1"> <i className="fa fa-car"></i> Order Now</button>
                                     <button className="btn btn-primary m-1"> <i className="fa fa-heart"></i> Favourite</button>
