e180f09b0e4d8cc961c17ab611804faf2a2e2368
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-09-19 15:05:06 +0530

    Making product details page dynamic

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -6,7 +6,10 @@ import Product2 from "../../assets/images/product2.png";
 import Product3 from "../../assets/images/product3.png";
 import Product4 from "../../assets/images/product4.png";
 
-function ProductDetails() {
+function ProductDetails(props) {
+
+    const productData = props.data;
+
     return (
         <>
             <Container className="BetweenTwoSection" fluid={true}>
@@ -14,64 +17,36 @@ function ProductDetails() {
                     <Col className="shadow-sm bg-white pb-3 mt-4" md={12} lg={12} sm={12} xs={12}>
                         <Row>
                             <Col className="p-3" md={6} lg={6} sm={12} xs={12}>
-                                <img className="w-100" src={Product1} />
+                                <img className="w-100" src={productData['productDetails'][0]['image_one']} />
                                 <Container className="my-3">
                                     <Row>
                                         <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                                            <img className="w-100" src={Product1} />
+                                            <img className="w-100" src={productData['productDetails'][0]['image_one']} />
                                         </Col>
                                         <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                                            <img className="w-100" src={Product2} />
+                                            <img className="w-100" src={productData['productDetails'][0]['image_two']} />
                                         </Col>
                                         <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                                            <img className="w-100" src={Product3} />
+                                            <img className="w-100" src={productData['productDetails'][0]['image_three']} />
                                         </Col>
                                         <Col className="p-0 m-0" md={3} lg={3} sm={3} xs={3}>
-                                            <img className="w-100" src={Product4} />
+                                            <img className="w-100" src={productData['productDetails'][0]['image_four']} />
                                         </Col>
                                     </Row>
                                 </Container>
                             </Col>
                             <Col className="p-3 " md={6} lg={6} sm={12} xs={12}>
-                                <h5 className="Product-Name">ASUS TUF A15 FA506IU Ryzen 7 4800H GTX</h5>
-                                <h6 className="section-sub-title">Some Of Our Exclusive Collection, You May Like Some Of Our Exclusive Collectio</h6>
+                                <h5 className="Product-Name">{productData['productList'][0]['title']}</h5>
+                                <h6 className="section-sub-title">{productData['productDetails'][0]['short_description']}</h6>
                                 <div className="input-group">
-                                    <div className="Product-price-card d-inline ">Reguler Price 200</div>
+                                    <div className="Product-price-card d-inline ">Reguler Price ${productData['productList'][0]['price']}</div>
                                     <div className="Product-price-card d-inline ">50% Discount</div>
-                                    <div className="Product-price-card d-inline ">New Price 100</div>
+                                    <div className="Product-price-card d-inline ">New Price ${productData['productList'][0]['special_price']}</div>
                                 </div>
-                                <h6 className="mt-2">Choose Color</h6>
-                                <div className="input-group">
-                                    <div className="form-check mx-1">
-                                        <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
-                                        <label className="form-check-label" htmlFor="exampleRadios1">Black</label>
-                                    </div>
-                                    <div className="form-check mx-1">
-                                        <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
-                                        <label className="form-check-label" htmlFor="exampleRadios1">Green</label>
-                                    </div>
-                                    <div className="form-check mx-1">
-                                        <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
-                                        <label className="form-check-label" htmlFor="exampleRadios1">Red</label>
-                                    </div>
-                                </div>
-
-                                <h6 className="mt-2">Choose Size</h6>
-                                <div className="input-group">
-                                    <div className="form-check mx-1">
-                                        <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
-                                        <label className="form-check-label" htmlFor="exampleRadios1">X</label>
-                                    </div>
-                                    <div className="form-check mx-1">
-                                        <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
-                                        <label className="form-check-label" htmlFor="exampleRadios1">XX</label>
-                                    </div>
-                                    <div className="form-check mx-1">
-                                        <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" />
-                                        <label className="form-check-label" htmlFor="exampleRadios1">XXXX</label>
-                                    </div>
-                                </div>
-
+                                <h6 className="mt-2">Category: <b>{productData['productList'][0]['category']}</b></h6>
+                                <h6 className="mt-2">Sub Category: <b>{productData['productList'][0]['subcategory']}</b></h6>
+                                <h6 className="mt-2">Brand: <b>{productData['productList'][0]['brand']}</b></h6>
+                                <h6 className="mt-2">Product Code: <b>{productData['productList'][0]['product_code']}</b></h6>
                                 <h6 className="mt-2">Quantity</h6>
                                 <input className="form-control text-center w-50" type="number" />
 
@@ -86,8 +61,7 @@ function ProductDetails() {
                         <Row>
                             <Col className="" md={6} lg={6} sm={12} xs={12}>
                                 <h6 className="mt-2">DETAILS</h6>
-                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation</p>
-                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation</p>
+                                {productData['productDetails'][0]['long_description']}
                             </Col>
 
                             <Col className="" md={6} lg={6} sm={12} xs={12}>
