70bb2e142f3a064fe50efdfea4e92820aecce528
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2023-12-23 15:08:22 +0530

    Favourites moved inside login

--- src/components/common/NavMenuDesktop.js
+++ src/components/common/NavMenuDesktop.js
@@ -71,14 +71,14 @@ function NavMenuDesktop(props) {
                                 </div>
                             </Col>
                             <Col className='p-1 mt-1' lg={4} md={4} sm={12} xs={12}>
-                                <Link to="/favourite" className='btn'>
-                                    <i className='fa h4 fa-heart'></i><sup><span className='badge text-white bg-danger'>3</span></sup>
-                                </Link>
                                 <Link to="/notification" className='btn'>
                                     <i className='fa h4 fa-bell'></i><sup><span className='badge text-white bg-danger'>5</span></sup>
                                 </Link>
                                 {isLoggedIn ? (
                                     <>
+                                        <Link to="/favourite" className='btn'>
+                                            <i className='fa h4 fa-heart'></i><sup><span className='badge text-white bg-danger'>3</span></sup>
+                                        </Link>
                                         <Link to="/profile" className="h4 btn">
                                             PROFILE
                                         </Link>
